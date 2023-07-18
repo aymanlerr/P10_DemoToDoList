148b6fac46e9ae7ab21d5304e12c6432fed8e4b5
22015131 <22015131@myrp.edu.sg>
2023-07-18 13:07:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22015131/p10_demotodolist/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.id22015131.p10_demotodolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvToDo;
+    CustomAdapter adapter;
+    ArrayList<ToDoItem> alToDo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvToDo = findViewById(R.id.lvToDo);
+        alToDo = new ArrayList<>();
+
+        Calendar date1 = Calendar.getInstance();
+        date1.set(2020, 8, 1);
+
+        Calendar date2 = Calendar.getInstance();
+        date2.set(2020, 8, 2);
+
+        alToDo.add(new ToDoItem("Go for movie", date1));
+        alToDo.add(new ToDoItem("Go for haircut", date2));
+
+        adapter = new CustomAdapter(this, R.layout.row, alToDo);
+        lvToDo.setAdapter(adapter);
+    }
+}
